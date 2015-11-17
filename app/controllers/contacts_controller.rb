ecbc5f773ff0e0888826fa9f794b379bb63ef4b0
Charles De Barros <charlesdebarros@yahoo.co.uk>
2015-11-17 18:38:26 +0000

    Adding new.html.erb and create.htmk.erb pages for the #contacts controller.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,17 @@
 class ContactsController < ApplicationController
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(params[:contact])
+    @contact.request = request
+      if @contact.deliver
+        flash[:error] = nil
+      else
+        flash[:error] = 'Unable to send message'
+        render :new
+      end
+  end
 end
